74abb2ec43ba024c44829a65129fb4382a3692d7
Sven Weiss <sven.weiss@icloud.com>
2015-09-03 19:35:06 +0200

    added new wetsuit view

---
+++ SurfLogbook/EnumSkyConditions.swift
@@ -0,0 +1,9 @@
+//
+//  EnumSkyConditions.swift
+//  SurfLogbook
+//
+//  Created by Sven Weiss on 25.08.15.
+//  Copyright (c) 2015 W&K Druckluftanlagenbau GmbH. All rights reserved.
+//
+
+import Foundation
---
+++ SurfLogbook/EnumThickness.swift
@@ -0,0 +1,9 @@
+//
+//  EnumThickness.swift
+//  SurfLogbook
+//
+//  Created by Sven Weiss on 02.09.15.
+//  Copyright © 2015 W&K Druckluftanlagenbau GmbH. All rights reserved.
+//
+
+import Foundation
